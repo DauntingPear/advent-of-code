c0fbe5a39e8a448f4916674f76d5d5bac9d72992
Adrian Brady <ajbrady999@protonmail.com>
2024-04-16 21:32:58 -0500

    final implementation

--- 2023/Day_1/C/Day_1.c
+++ 2023/Day_1/C/Day_1.c
@@ -1,23 +1,58 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  char *filename = "input.txt";
+  char *filename = "../input.txt";
   FILE *fp = fopen(filename, "r");
 
   if (fp == NULL) {
-    printf("Error: could not open file %s", filename);
+    // printf("Error: could not open file %s", filename);
     return 1;
   }
 
-  const unsigned MAX_LENGTH = 256;
-  char buffer[MAX_LENGTH];
+  int character;
+  char first_num;
+  char last_num;
+  bool nl = true;
+  char num[2];
+  int total = 0;
 
-  while (fgets(buffer, MAX_LENGTH, fp)) {
-    printf("%s", buffer);
+  // Need to iterate over current line
+  // get first and last number characters
+  // and concat them. Then, convert those numbers into
+  // an integer. Add that number to some total variable.
+  // Print that variable at end of program for result.
+  while ((character = fgetc(fp)) != EOF) {
+    // printf("Character: %c, %d | %d\n", character, character, nl);
+    if (character == '\n') {
+      total += num[0];
+      total += num[1];
+      // printf("Num: %d%d\n", num[0] / 10, num[1]);
+      num[0] = 0;
+      num[1] = 0;
+      // printf("<<Newline>>\n");
+      nl = true;
+      continue;
+    }
+    switch (character) {
+    case 48 ... 57:
+      if (nl == true) {
+        nl = false;
+        num[1] = character - 48;
+        num[0] = num[1] * 10;
+      } else {
+        num[1] = character - 48;
+      }
+      nl = false;
+      break;
+    default:
+      break;
+    }
   }
 
+  printf("Input file -> %s\n", filename);
+  printf("Total: %d\n", total);
   fclose(fp);
-
   return 0;
 }
